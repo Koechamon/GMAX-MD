87d551b3aa34f4448d8e5e9326b33c69723b0e87
Koechamon <kipngetichamon171@gmail.com>
2025-01-23 13:43:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your gmax md session id here",
+SESSION_ID: process.env.SESSION_ID || "GMAX-MD~eMZGUDTQ#TOJsCdlb9ousqFw4jt4ycWMET_Y8dXTFjaBOsxxL1r8",
  
 PREFIX: process.env.PREFIX || ".",
 // Enter Your Desired Prefix
 
-BOT_NAME: process.env.BOT_NAME || "『 𝐆𝐌𝐀𝐗 𝐌𝐃 』",
+BOT_NAME: process.env.BOT_NAME || "『 KOECH-AK 』",
 // Enter Your Bot Name
 
 ANTI_VV: process.env.ANTI_VV || "true",
@@ -25,13 +25,13 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255622053093",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "254762469872",
 // Set Owner Name
 
-OWNER_NAME: process.env.OWNER_NAME || "sɪʀ ɢᴍᴀx",
+OWNER_NAME: process.env.OWNER_NAME || "AMON*AK",
 // Set Footer
 
-DESCRIPTION: process.env.DESCRIPTION || "*© Gᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ɢᴍᴀx*",
+DESCRIPTION: process.env.DESCRIPTION || "*© Hello×*",
 // add bot owner name   
  
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/6BMJmGY/mrfrankofc.jpg",
@@ -49,16 +49,16 @@ AUTO_REACT: process.env.AUTO_REACT || "false",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 // false or true for anti bad words
 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
 
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS__MSG || "*[❄️] Hi there, GMAX-MD viewed your Status🎐*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS__MSG || "*[❄️] Hi there, KOECH-AK  viewed your Status🎐*",
 
 // set the auto reply massage on status reply    
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 // make bot public-private-inbox-group 
 
 ANTI_LINK: process.env.ANTI_LINK || "true",
@@ -70,10 +70,10 @@ AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
 
-AUTO_REPLY: process.env.AUTO_REPLY || "true",
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply
  
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 // maks true for always online 
 
 PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
